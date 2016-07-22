ff4ac6acdc8a362030212a4d42d8ffca9cfe216d
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2016-07-23 08:25:46 +0900

    add method prepare_output_layer

--- deeplearning/NNetwork.py
+++ deeplearning/NNetwork.py
@@ -50,16 +50,19 @@ class Layer():
 class NNetwork():        
     def __init__(self,layers):
         self.layers=layers
+    def _register_weights(self):
+        self.ws=[lay.get_w for lay in self.layers]
+    def _register_bias(self):
+        self.bs=[lay.get_b for lay in self.layers]
 
-def prepare_input_layer():
+def prepare_input_param():
     #3 by 3 identity matrix
     w=np.identity(3)
     #3 by 1 zero vector  
     b=np.zeros([3,1]) 
-    input_layer=Layer(identity_func,w,b)
-    return input_layer    
+    return w,b,identity_func    
 
-def prepare_test_param():
+def prepare_mediant_param():
     w=np.array(([0,-1,1],
                 [1,0,1],
                 [0,0,1]))
@@ -69,7 +72,7 @@ def prepare_test_param():
                 1]).reshape((-1,1))
     return w,b,sigmoid
         
-def prepare_random_param():
+def prepare_mediant_param_random():
     
     #make 3 by 3 matrix
     w=np.random.rand(3,3)
@@ -78,19 +81,34 @@ def prepare_random_param():
     #initialize input value
     return w,b,sigmoid
 
+def prepare_output_param():
+    w=np.array(([0,-1,1],
+                [1,0,1],
+                [0,0,1]))
+    #set b and x as column vector
+    b=np.array([1,
+                1,
+                1]).reshape((-1,1))
+    return w,b,sigmoid
+
+def prepare_input_layer():
+    w,b,id_func=prepare_input_param()
+    input_layer=Layer(id_func,w,b)
+    return  input_layer
+
 def prepare_mediant_layer(input_layer):
-    w,b,sig=prepare_test_param()
+    w,b,sig=prepare_mediant_param()
     mediant_layer=Layer(sig,w,b)
     mediant_layer.set_unit_from_previous_layer(input_layer.pass_next_layer())
     return mediant_layer
 
 def prepare_output_layer(mediant_layer):
-    w,b,sig=prepare_test_param()
+    w,b,sig=prepare_output_param()
     output_layer=Layer(sig,w,b)
     output_layer.set_unit_from_previous_layer(mediant_layer.pass_next_layer())
     return output_layer
 
-def get_output_value(x):
+def network_output(x):
     input_layer=prepare_input_layer()
     input_layer.set_unit(x)
     print("input_layer=\n%s"%input_layer)
@@ -98,13 +116,15 @@ def get_output_value(x):
     print("mediant_layer=\n%s"%mediant_layer)
     output_layer=prepare_output_layer(mediant_layer)
     print("output_layer=\n%s"%output_layer)
+    return output_layer.pass_next_layer()
     
 def main():
     #create 3 by 1 vector
     input_value=(np.array([1,
-                 2,
-                 3]).reshape((-1,1)))
-    get_output_value(input_value)
-                    
+                           2,
+                           3]).reshape((-1,1)))
+    output_value=network_output(input_value)
+    print("output_value=\n%s"%output_value)               
+    
 if __name__=='__main__':
     main()
