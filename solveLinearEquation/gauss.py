d48cce3ef38a2379defbdc55195495dd17d5ff8d
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2016-09-16 13:22:31 +0900

    [fix code] remove return

--- solveLinearEquation/gauss.py
+++ solveLinearEquation/gauss.py
@@ -1,6 +1,4 @@
 import numpy as np 
-
-
 def solveeq_gauss(a,b,x):
     """
     solve equation ax=b using simple gauss method
@@ -14,7 +12,7 @@ def solveeq_gauss(a,b,x):
             p= a[i,j] /a[j,j]
             a[i]-=p*a[j]
     #back forward
-    for i in range(row)[::-1]:
+    for i in range(row)[::-1]:#reverse iteration
         x[i]=(a[i,col-1]-np.dot(a[i,i+1:row],x[i+1:row]))/a[i,i]
 
 def main():
@@ -23,7 +21,6 @@ def main():
                  [4,1,-2]],dtype='float64')
     b=np.matrix([28,7,21],'float64').T
     x=np.empty(3)
-
     solveeq_gauss(a,b,x)
     print(x)
 if __name__ == '__main__':
