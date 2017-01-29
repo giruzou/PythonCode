984a8487319b5bb905529126d69eecc01489e590
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2017-01-29 20:55:08 +0900

    fix code

--- fourierAnalysis/fourier_series.py
+++ fourierAnalysis/fourier_series.py
@@ -4,13 +4,10 @@ from scipy import integrate
 
 Pi=np.pi
 T=2*Pi
-N= 90
+N= 50
 
-naive=None
-approx_series=None
-explicit_series=None
-semi_explicit=None
-a_func=None
+target_func=None
+fourier_series=None
 
 def getVarName( var, symboltable, error=None ) :
     """
@@ -28,17 +25,22 @@ def getVarName( var, symboltable, error=None ) :
 def step_func(xs):
 	return np.where(xs<0,0,1)
 
-def plot_func(funcs):
+def linear(xs,a=1):
+	return a*xs
+
+def plot_func(funcs,func_name="function"):
 	fig,ax=plt.subplots()
 	for func in funcs:
-		xs=np.linspace(-T/2,T/2,100)
+		xs=np.linspace(-T/2,T/2,5000)
 		ys=func[0](xs)
 		ax.plot(xs,ys,label=func[1])
 	ax.legend()
-	plt.savefig("fourier.png")
+	ax.set_title("target_func (= "+func_name+" ) and its fourier series")
+	plt.savefig(func_name+".png")
+	plt.show()
 
-def get_fourier(func):
-	ts=np.linspace(-T/2,T/2,1000)
+def get_fourier(func,func_name):
+	ts=np.linspace(-T/2,T/2,5000)
 	A0=integrate.simps((lambda t:func(t)/T)(ts),ts) 
 	As=np.array([integrate.simps((lambda t:2*func(t)*np.cos(2*n*Pi*t/T)/T)(ts),ts) for n in range(1,N)],dtype=np.float64)
 	Bs=np.array([integrate.simps((lambda t:2*func(t)*np.sin(2*n*Pi*t/T)/T)(ts),ts) for n in range(1,N)],dtype=np.float64)
@@ -47,16 +49,13 @@ def get_fourier(func):
 	cos_part=lambda t : np.array([np.cos(2*n*Pi*t/T) for n in range(1,N)],dtype=np.float64)
 	sin_part=lambda t : np.array([np.sin(2*n*Pi*t/T) for n in range(1,N)],dtype=np.float64)
 
-	global naive,approx_series,explicit_series,semi_explicit,a_func
-	naive=lambda t: A0+As.dot(cos_part(t))+Bs.dot(sin_part(t))
-	approx_series=lambda t: A0+Bs.dot(sin_part(t))
-	semi_explicit=lambda t: A0+Explicit_Bs.dot(sin_part(t))
-
-	explicit_series=lambda t: 1/2 + sum([2*np.sin((2*m-1)*t)/((2*m-1)*Pi) for m in range(1,N)])
-	a_func=lambda t:As.dot(cos_part(t))
-	plot_func((func,getVarName(func,globals())) for func in [step_func,naive,approx_series,explicit_series,semi_explicit,a_func])
+	global target_func,fourier_series
+	target_func=func
+	fourier_series=lambda t: A0+As.dot(cos_part(t))+Bs.dot(sin_part(t))
+	plot_func([(func,getVarName(func,globals())) for func in [func,fourier_series]],func_name)
 
 def main():
-	get_fourier(step_func)
+	get_fourier(step_func,getVarName(step_func,globals()))
+	get_fourier(linear,getVarName(linear,globals()))
 if __name__ == '__main__':
 	main()
