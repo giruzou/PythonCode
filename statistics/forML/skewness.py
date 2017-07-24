a28e7646f194f698f99bc73da8a9e6f3a1a490fa
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2017-07-24 12:48:04 +0900

    add expectation

--- statistics/forML/skewness.py
+++ statistics/forML/skewness.py
@@ -1,6 +1,26 @@
 from matplotlib import pyplot as plt 
 import numpy as np
 from scipy import stats
+
+def prob_function(sample):
+    values=[]
+    for i in [0,1,2,3,4,5]:
+        value=np.where(np.array(sample)==i)
+        values.append(len(value[0])/len(sample))
+    return lambda i : values[i]
+
+def expectation(func,var=lambda x:x):
+    return sum([var(x)*func(x) for x in [0,1,2,3,4,5]])
+
+def variance(func,var=lambda x:x):
+    return expectation(func,lambda x:(x-expectation(func))**2)
+
+def std_deviation(func,var=lambda x:x):
+    return (variance(func,var))**0.5
+
+
+
+
 def main():
     sample1=2*[0]+4*[1]+12*[2]+24*[3]+24*[4]+10*[5]
     sample2=6*[0]+14*[1]+18*[2]+18*[3]+14*[4]+6*[5]
@@ -15,8 +35,20 @@ def main():
     for i in [1,2,3]:
         eval("ax{0}.set_title(\
                 'skew=%f'%stats.skew(sample{0}))".format(i))
-        eval("ax{0}.hist(sample{0})".format(i))
-    
+        #eval("ax{0}.hist(sample{0})".format(i))
+        sample=eval("sample{}".format(i))
+        func=prob_function(sample)
+        func=np.vectorize(func)
+        print(expectation(func))
+        print(variance(func))
+        print(np.var(sample))
+        print(std_deviation(func))
+        print(np.std(sample))
+        xs=np.array([0,1,2,3,4,5])
+        ys=func(xs)
+        print(ys)
+        eval("ax{0}.plot(xs,ys)".format(i))
+
     plt.show()
 
 if __name__ == '__main__':
