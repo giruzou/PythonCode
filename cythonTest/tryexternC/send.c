525a07d5f97c7ca3fbd602968e3c83f0b44bfcde
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2017-09-29 21:29:48 +0900

    add \n

--- cythonTest/tryexternC/send.c
+++ cythonTest/tryexternC/send.c
@@ -2,6 +2,6 @@
 #include "func.h"
 
 int CalcSomething(int a, int b){
-    printf("a=%d, b=%d, Func(a,b)=%d",a,b,Func(a,b));
+    printf("a=%d, b=%d, Func(a,b)=%d\n",a,b,Func(a,b));
     return Func(a,b);
 }
