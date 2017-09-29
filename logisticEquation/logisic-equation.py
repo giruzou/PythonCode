ec6553d9f1fdf3dd77c7b7d0499782ad79774800
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2017-09-29 21:31:01 +0900

    apply pep 8

--- logisticEquation/logisic-equation.py
+++ logisticEquation/logisic-equation.py
@@ -1,41 +1,43 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-delta=0.1
 
-def get_integral_curve(f,init_xy,end_x):
-    """solve ode 'dy/dx=f(x,y)'
+def get_integral_curve(f, init_xy, x_end, delta):
     """
-    (x,y)=init_xy
-    xs,ys=[x],[y]
-    for i in np.arange(init_xy[0],end_x,delta):
-        y+=delta*f((x,y))
-        x+=delta
+    solve ode 'dy/dx=f(x,y)' with Euler method
+    """
+    (x, y) = init_xy
+    xs, ys = [x], [y]
+    for i in np.arange(init_xy[0], x_end, delta):
+        y += delta*f(x, y)
+        x += delta
         xs.append(x)
         ys.append(y)
-    return xs,ys
+    return xs, ys
+
 
 def main():
-    """solve logistic equation dy/dx=y(1-y)
     """
-    #set initial values of x and y
-    init_xy=(0,0.5)
-    end_x=4
-
-    fig,ax=plt.subplots()
-
-    #show integral curve of logistic equation 
-    xs,ys=get_integral_curve(lambda (x,y):y*(1-y),init_xy,end_x)
-    ax.plot(xs,ys,"o",color='blue')
-
-    #show exact solution
-    sigmoid_xs=np.arange(-5,5,delta)
-    sigmoid_ys=1/(1+np.exp(-sigmoid_xs))
-    ax.plot(sigmoid_xs,sigmoid_ys,"-",color='red')
-    
-    ax.set_xlim([-5,5])
-    ax.set_ylim([-0.5,1.5])
+    solve logistic equation dy/dx=y(1-y)
+    """
+    # set parameters
+    init_xy = (0, 0.5)
+    x_end = 5
+    delta = 0.1
+    # calc integral curve of logistic equation
+    f = lambda x, y: y*(1-y)
+    xs, ys = get_integral_curve(f, init_xy, x_end, delta)
+    # calc explicit solution
+    sigmoid_xs = np.arange(-5, 5, delta)
+    sigmoid_ys = 1/(1+np.exp(-sigmoid_xs))
+    # plot
+    fig, ax = plt.subplots()
+    ax.plot(xs, ys, "x", color='blue', label="integral curve")
+    ax.plot(sigmoid_xs, sigmoid_ys, "-", color='red', label='explicit curve')
+    ax.legend()
+    ax.set_xlim([-5, 5])
+    ax.set_ylim([-0.5, 1.5])
     plt.show()
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
--- solveLinearEquation/gauss.py
+++ solveLinearEquation/gauss.py
@@ -1,42 +1,66 @@
-import numpy as np 
+import numpy as np
 import sympy as sy
-def solveeq_gauss(a,b,x):
+
+
+def solveeq_gauss(a, b, x):
     """
     solve equation ax=b using simple gauss method
     """
-    #define extended coefficient matrix as 'a'
-    a=np.concatenate([a,b],axis=1)
-    #push forward
-    (row,col)=a.shape
+    # define extended coefficient matrix as 'a'
+    a = np.concatenate([a, b], axis=1)
+    # push forward
+    (row, col) = a.shape
     for j in range(row):
-        for i in range(j+1,row):
-            p= a[i,j] /a[j,j]
-            a[i]-=p*a[j]
-    #back forward
-    for i in range(row)[::-1]:#reverse iteration
-        x[i]=(a[i,col-1]-np.dot(a[i,i+1:row],x[i+1:row]))/a[i,i]
-        
-def confirm_numpy(a,b,x):
-    x=np.linalg.inv(a)@b
+        print("a=\n", a)
+        for i in range(j+1, row):
+            p = a[i, j] / a[j, j]
+            a[i] -= p*a[j]
+    # back forward
+    for i in range(row)[::-1]:  # reverse iteration
+        x[i] = (a[i, col-1]-np.dot(a[i, i+1:row], x[i+1:row]))/a[i, i]
 
-def confirm_sympy(a,b,x):
-    x=a.solve(b)
+
+def confirm_numpy(a, b, x):
+    x = np.linalg.inv(a)@b
+
+
+def confirm_sympy(a, b, x):
+    a = sy.Matrix(a)
+    x = a.solve(b)
+
+
+def verity(a, b, x):
+    solveeq_gauss(a, b, x)
+    print("gauss x:\n{}".format(x))
+    confirm_numpy(a, b, x)
+    print("numpy x:\n{}".format(x))
+    confirm_sympy(a, b, x)
+    print("sympy.solve x:\n{}".format(x))
+
+
+def test1():
+    a = np.matrix([[2, 4, 6],
+                   [1, -1, 5],
+                   [4, 1, -2]], dtype='float64')
+    b = np.matrix([28, 7, 21], 'float64').T
+    x = np.empty(b.shape)
+    verity(a, b, x)
+
+
+def test2():
+
+    a = np.matrix([[2, 4, 5, 2],
+                   [1, -8, 2, -6],
+                   [4, 1, -10, -2],
+                   [1, 7, 1, -2]], dtype='float64')
+    b = np.matrix([9, -3, 1, -3], 'float64').T
+    x = np.empty(b.shape)
+    verity(a, b, x)
+
 
 def main():
-    a=np.matrix([[2,4,6],
-                 [1,-1,5],
-                 [4,1,-2]],dtype='float64')
-    b=np.matrix([28,7,21],'float64').T
-    x=np.empty(3)
-    solveeq_gauss(a,b,x)
-    print("gauss x:{}".format(x))
-    confirm_numpy(a,b,x)
-    print("numpy x{}".format(x))
-    a=sy.Matrix(a)
-    confirm_sympy(a,b,x)
-    print("sympy.solve x:{}".format(x))
-
-
+    test1()
+    test2()
 
 if __name__ == '__main__':
     main()
