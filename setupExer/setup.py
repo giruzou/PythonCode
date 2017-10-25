6a1b3ad09e06938fcf182cc0ae15cbe519e71ac8
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2017-10-25 17:22:57 +0900

    add setup example

---
+++ setupExer/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='myexample',
+      version='1.0',
+      description="Trivial Package. Distribution Test Only",
+      author='Satoshi Tetasaki',
+      author_email="terasakisatoshi.math@gmail.com",
+      packages=['myexample'])
